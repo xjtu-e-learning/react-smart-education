57ab8ef644ed335fbb14b7c5bedbd01ee46e912d
wkw307 <wkw307@gmail.com>
2018-08-10 16:57:58 +0800

    更新目录结构

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App.jsx';
+import App from './views/App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
