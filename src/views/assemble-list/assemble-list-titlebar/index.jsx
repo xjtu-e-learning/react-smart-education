a4de18a7c7fe22d6b59d0a8ae0c35923bda3b358
wkw307 <wkw307@gmail.com>
2018-08-22 20:30:24 +0800

    显示选中主题或分面的碎片数量

--- src/views/assemble-list/assemble-list-titlebar/index.jsx
+++ src/views/assemble-list/assemble-list-titlebar/index.jsx
@@ -3,16 +3,20 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import { withStyles } from '@material-ui/core/styles';
+import { inject, observer } from 'mobx-react';
 
 const styles = theme => ({});
 
+@inject('appState')
+@observer
 class AssembleListTitlebar extends React.Component {
   render() {
+    const { appState } = this.props;
     return (
       <AppBar position="static" color="default">
         <Toolbar>
           <Typography variant="title" color="inherit">
-            碎片数量
+            碎片数量： {appState.currentAssembleList.length}
           </Typography>
         </Toolbar>
       </AppBar>
