55e89364efb12d56ba1e7e5347a5b48cbf489515
wkw307 <wkw307@gmail.com>
2018-10-23 10:44:19 +0800

    重新设计recommendation-button

--- src/views/recommendation-list/recommendation-button/index.jsx
+++ src/views/recommendation-list/recommendation-button/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -7,6 +8,8 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { inject, observer } from 'mobx-react';
 import { post_log_of_mouseclick_recommendation } from '../../../log/post-log-SDK';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
   root: {
@@ -22,6 +25,17 @@ const styles = theme => ({
   },
   selectEmpty: {
     marginTop: theme.spacing.unit * 2
+  },
+  select: {
+    borderColor: 'white'
+  },
+  button: {
+    margin: theme.spacing.unit,
+    color: 'white',
+    borderColor: 'white'
+  },
+  focused: {
+    '&$focused': { color: 'white' }
   }
 });
 
@@ -29,8 +43,15 @@ const styles = theme => ({
 @observer
 class RecommendationButton extends React.Component {
   state = {
-    name: ''
+    name: '零基础',
+    labelWidth: 0
   };
+
+  componentDidMount() {
+    this.setState({
+      labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth
+    });
+  }
 
   handleChange = (studentCode, courseId, domainName, event) => {
     this.setState({ name: event.target.value });
@@ -46,10 +67,9 @@ class RecommendationButton extends React.Component {
     let courseId = appState.courseId;
     let domainName = appState.domainName.get();
     const recnames = [
-      '最短学习路径',
-      '有效学习路径',
-      '补全学习路径',
-      '热度学习路径'
+      '零基础',
+      '自定义学习',
+      '速成学习'
     ];
     if (appState.recommendationList.get() !== undefined && appState.currentTopic.topicId === -1) {
       // console.log('test');
@@ -58,22 +78,58 @@ class RecommendationButton extends React.Component {
     }
     return (
       <form className={classes.root} autoComplete="off">
-        <FormControl className={classes.formControl}>
-          <InputLabel className={classes.white} htmlFor="name-simple">
-            主题推荐方式
+        {/*<FormControl className={classes.formControl}>*/}
+        {/*<InputLabel className={classes.white} htmlFor="name-simple">*/}
+        {/*主题推荐方式*/}
+        {/*</InputLabel>*/}
+        {/*<Select*/}
+        {/*value={this.state.name}*/}
+        {/*onChange={this.handleChange.bind(this, studentCode, courseId, domainName)}*/}
+        {/*className={classes.white}*/}
+        {/*>*/}
+        {/*{recnames.map(recname => (*/}
+        {/*<MenuItem value={recname} key={recname}>*/}
+        {/*{recname}*/}
+        {/*</MenuItem>*/}
+        {/*))}*/}
+        {/*</Select>*/}
+        {/*</FormControl>*/}
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel
+            ref={ref => {
+              this.InputLabelRef = ref;
+            }}
+            htmlFor="outlined-age-native-simple"
+            className={classes.white}
+            FormLabelClasses={{ focused: classes.focused }}
+          >
+            学习方式
           </InputLabel>
           <Select
             value={this.state.name}
             onChange={this.handleChange.bind(this, studentCode, courseId, domainName)}
+            input={
+              <OutlinedInput
+                name="recname"
+                labelWidth={this.state.labelWidth}
+                id="outlined-age-native-simple"
+                classes={{ notchedOutline: classes.select }}
+              />
+            }
             className={classes.white}
+            classes={{ icon: classes.white }}
           >
             {recnames.map(recname => (
-              <MenuItem value={recname} key={recname}>
-                {recname}
-              </MenuItem>
+              <MenuItem value={recname} key={recname}>{recname}</MenuItem>
             ))}
           </Select>
         </FormControl>
+        <Button variant="outlined" className={classes.button} style={{ width: 86 }}>
+          确认
+        </Button>
+        <Button variant="outlined" className={classes.button} style={{ width: '100%' }}>
+          选择知识主题
+        </Button>
       </form>
     );
   }
