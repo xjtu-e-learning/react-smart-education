14efbe3c1716b357500d6aa727e6cfdda225d8e5
wkw307 <wkw307@gmail.com>
2018-11-06 11:06:24 +0800

    添加课程切换

--- src/views/titlebar/course-info/index.jsx
+++ src/views/titlebar/course-info/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { inject, observer } from 'mobx-react';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { Menu, Dropdown } from 'antd';
 
 const styles = theme => ({
   right: {
@@ -33,6 +34,17 @@ const styles = theme => ({
   }
 });
 
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target="_self" rel="noopener noreferrer" href="?courseId=201&studentCode=3117033016">数据结构</a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_self" rel="noopener noreferrer" href="?courseId=5&studentCode=3117033016">Java(video course)</a>
+    </Menu.Item>
+  </Menu>
+);
+
 @inject('appState')
 @observer
 class CourseInfo extends React.Component {
@@ -42,7 +54,11 @@ class CourseInfo extends React.Component {
     const topicNum = this.props.appState.topicList.get() !== undefined ? this.props.appState.topicList.get().length : 0;
     return (
       <div className={classes.right}>
-        <span>课程：<label className={classes.blue}>{this.props.appState.domainName.get()}</label></span>
+        <span>课程：
+          <Dropdown overlay={menu}>
+            <label className={classes.blue}>{this.props.appState.domainName.get()}</label>
+          </Dropdown>
+        </span>
         <span className={classes.span}>知识主题：<label className={classes.orange}>{topicNum}</label></span>
         <span className={classes.span}><AccountCircleIcon className={classes.icon}/> Yotta同学</span>
       </div>
