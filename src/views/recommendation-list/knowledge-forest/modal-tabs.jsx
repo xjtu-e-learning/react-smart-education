88ccaeb1eacd99e82e6727997923fabd68a9f74d
wkw307 <wkw307@gmail.com>
2018-11-08 16:34:45 +0800

    零基础学习方式不显示知识森林推荐路径

--- src/views/recommendation-list/knowledge-forest/modal-tabs.jsx
+++ src/views/recommendation-list/knowledge-forest/modal-tabs.jsx
@@ -7,12 +7,13 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import KnowledgeForestModal from './knowledge-forest-modal';
 import KnowledgeForestRecommendation from './knowledge-forest-recommendation';
+import { inject, observer } from 'mobx-react';
 
 function TabContainer(props) {
   return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
+    <div style={{ padding: 8 * 3 }}>
       {props.children}
-    </Typography>
+    </div>
   );
 }
 
@@ -27,6 +28,8 @@ const styles = theme => ({
   }
 });
 
+@inject('appState')
+@observer
 class ModalTabs extends React.Component {
   state = {
     value: 0
@@ -35,6 +38,10 @@ class ModalTabs extends React.Component {
   handleChange = (event, value) => {
     this.setState({ value });
   };
+
+  componentWillMount() {
+    if (this.props.appState.currentRecommendation === '零基础' || this.props.appState.chosenTopic.topicId === -1) this.setState({ value: 0 });
+  }
 
   render() {
     const { classes } = this.props;
@@ -45,7 +52,8 @@ class ModalTabs extends React.Component {
         <AppBar position="static">
           <Tabs value={value} onChange={this.handleChange} centered>
             <Tab label="认知路径"/>
-            <Tab label="推荐路径"/>
+            {this.props.appState.currentRecommendation !== '零基础' && this.props.appState.chosenTopic.topicId !== -1 &&
+            <Tab label="推荐路径"/>}
           </Tabs>
         </AppBar>
         {value === 0 && <TabContainer><KnowledgeForestModal/></TabContainer>}
